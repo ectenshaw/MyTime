f84659417f86f0a765ae4e9f4353ae76d2cd6480
Emily <etenshaw@radford.edu>
2017-04-27 10:24:37 -0400

    update 1

---
+++ app/src/main/kotlin/com/simplemobiletools/calendar/activities/ProfileScreen.java
@@ -0,0 +1,16 @@
+package com.simplemobiletools.calendar.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.simplemobiletools.calendar.R;
+
+
+public class ProfileScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
